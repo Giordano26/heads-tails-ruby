9b8ad3860d0fcff6b0127a1e265c96be48b376a2
Giordano26 <smfgiordano@gmail.com>
2023-03-31 10:57:42 -0300

    set game and main class

---
+++ src/game.rb
@@ -0,0 +1,17 @@
+require './player.rb'
+#require './menu.rb'
+#require './play.rb'
+
+class Game
+    def initialize
+        @player = Player.new
+        #@menu = Menu.new
+        #@play = Play.new
+    end
+
+    def start
+        
+
+    end
+
+end
---
+++ src/main.rb
@@ -0,0 +1,10 @@
+require './game.rb'
+
+class Main
+    game = Game.new
+
+    game.start()
+
+end
+
+
