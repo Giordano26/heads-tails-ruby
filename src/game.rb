679ba71dd5848293c7dabefa06394bebf8a41f6c
Stefano Giordano <stef@MacBook-Air-de-Stefano.local>
2023-04-01 19:39:41 -0300

    feat: menu added to game class

--- src/game.rb
+++ src/game.rb
@@ -1,16 +1,26 @@
 require './player.rb'
-#require './menu.rb'
+require './menu.rb'
 #require './play.rb'
 
 class Game
     def initialize
         @player = Player.new
-        #@menu = Menu.new
+        @menu = Menu.new("Play","About","Quit")
         #@play = Play.new
     end
 
     def start
-        
+        @menu.print_initial_menu
+        while((choice = @menu.get_menu_choice))
+            case choice 
+            when 1
+                "Let's play!"
+            when 2
+                @menu.print_about_game
+            when @menu.length_quit
+                exit(0)
+            end
+        end
 
     end
 
