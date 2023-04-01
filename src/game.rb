6c21f90ff3bed647093fee349a6cb2c928b64629
Stefano Giordano <stef@MacBook-Air-de-Stefano.local>
2023-04-01 20:22:46 -0300

    feat: player picks to play class parameter

--- src/game.rb
+++ src/game.rb
@@ -6,21 +6,41 @@ class Game
     def initialize
         @player = Player.new
         @menu = Menu.new("Play","About","Quit")
-        #@play = Play.new
+        @ht_menu = Menu.new("Heads","Tails","Quit")
     end
 
     def start
         @menu.print_initial_menu
-        while((choice = @menu.get_menu_choice))
+        while(choice = @menu.get_menu_choice)
             case choice 
             when 1
-                "Let's play!"
+                puts "Let's play!"
+                break
             when 2
                 @menu.print_about_game
             when @menu.length_quit
                 exit(0)
             end
         end
+
+        
+        @menu.print_choice_menu
+       
+        while(ht_choice = @ht_menu.get_menu_choice)
+            case ht_choice
+            when 1
+                puts "You picked heads!"
+                break
+            when 2
+                puts "You picked tails!"
+                ht_choice = 0  # h = 1 t = 0
+                break
+            when @ht_menu.length_quit
+                exit(0)
+            end
+        end
+
+        #@play = Play.new(ht_choice)
 
     end
 
