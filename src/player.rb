433eddb80efe524557b9c592b86a99538c813618
Stefano Giordano <stef@MacBook-Air-de-Stefano.local>
2023-05-23 19:59:04 -0300

    feat: attr for lifes

--- src/player.rb
+++ src/player.rb
@@ -2,7 +2,9 @@ require './scoreboard.rb'
 class Player
     
     attr_reader :playerscore
+    attr_accessor :lifes
     def initialize
         @playerscore = Scoreboard.new
+        @lifes = 3
     end
 end
